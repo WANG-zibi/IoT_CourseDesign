af9420195ff856280b6ec29ee6e63a01e774ffb1
kingskyuniverse <wtyxxs@foxmail.com>
2021-01-06 15:48:40 +0800

    串口通信test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,91 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLayout>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_serialPort = new QSerialPort();
+    initUI();
+    m_portNameList = getPortNameList();
+    m_PortNameComboBox->addItems(m_portNameList);
+    connect(m_OpenPortButton,&QPushButton::clicked,this,&MainWindow::openPort);
 }
 
 MainWindow::~MainWindow()
 {
+    if (m_serialPort->isOpen())
+    {
+        m_serialPort->close();
+    }
+    delete m_serialPort;
+
     delete ui;
 }
+
+void MainWindow::initUI()
+{
+    this->setWindowTitle("test QSerialPort");
+    m_OpenPortButton = new QPushButton();
+    m_OpenPortButton->setText("打开串口");
+    m_PortNameComboBox  = new QComboBox();
+    QHBoxLayout *m_layout = new QHBoxLayout();
+    m_layout->addWidget(m_PortNameComboBox);
+    m_layout->addWidget(m_OpenPortButton);
+    QWidget * widget = new QWidget ();
+    widget->setLayout(m_layout);
+    this-> setCentralWidget(widget) ;
+}
+
+QStringList MainWindow::getPortNameList()
+{
+    QStringList m_serialPortName;
+    foreach(const QSerialPortInfo &info,QSerialPortInfo::availablePorts())
+    {
+        m_serialPortName << info.portName();
+        qDebug()<<"serialPortName:"<<info.portName();
+    }
+    return m_serialPortName;
+}
+
+void MainWindow::openPort()
+{
+    if(m_serialPort->isOpen())//如果串口已经打开了 先给他关闭了
+    {
+        m_serialPort->clear();
+        m_serialPort->close();
+    }
+    m_serialPort->setPortName(m_PortNameComboBox->currentText());//当前选择的串口名字
+
+    if(!m_serialPort->open(QIODevice::ReadWrite))//用ReadWrite 的模式尝试打开串口
+    {
+        qDebug()<<"打开失败!";
+        return;
+    }
+    qDebug()<<"串口打开成功!";
+
+    m_serialPort->setBaudRate(QSerialPort::Baud115200,QSerialPort::AllDirections);//设置波特率和读写方向
+    m_serialPort->setDataBits(QSerialPort::Data8);      //数据位为8位
+    m_serialPort->setFlowControl(QSerialPort::NoFlowControl);//无流控制
+    m_serialPort->setParity(QSerialPort::NoParity); //无校验位
+    m_serialPort->setStopBits(QSerialPort::OneStop); //一位停止位
+
+    connect(m_serialPort,SIGNAL(readyRead()),this,SLOT(receiveInfo()));
+}
+
+//接收到单片机发送的数据进行解析
+void MainWindow::receiveInfo()
+{
+    QByteArray info = m_serialPort->readAll();
+
+    //m_serialPort->write();
+
+    qDebug()<<"receive info:"<<info;
+}
+
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QWidget>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QComboBox>
+#include <QPushButton>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,8 +18,18 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void initUI();
 
+    QStringList getPortNameList();//获取所有可用的串口列表
+
+    void openPort();//打开串口
+public slots:
+    void receiveInfo();
 private:
     Ui::MainWindow *ui;
+    QSerialPort* m_serialPort; //串口类
+    QStringList m_portNameList;
+    QComboBox* m_PortNameComboBox;
+    QPushButton* m_OpenPortButton;
 };
 #endif // MAINWINDOW_H
